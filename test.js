0ec226f636c8175e521af5fd04246374d7d4763a
Drew Gross <drew.a.gross@gmail.com>
2017-05-07 02:31:11 -0700

    Compile to c and run

--- test.js
+++ test.js
@@ -1,6 +1,10 @@
 import test from 'ava';
 
-import { parse, evaluate } from './compiler';
+import { parse, evaluate, toC } from './compiler';
+
+import tmp from 'tmp-promise';
+import fs from 'fs-extra';
+import { exec } from 'child-process-promise';
 
 test('parse empty file', t => {
     t.deepEqual(parse(''), {
@@ -18,4 +22,17 @@ test('parse single number', t => {
 
 test('evaluate single number', t => {
     t.deepEqual(evaluate(parse('7')), 7);
+});
+
+test('compile and run c', async t => {
+    const cFile = await tmp.file({ postfix: '.c'});
+    const exeFile = await tmp.file();
+    await fs.writeFile(cFile.fd, toC(parse('7')));
+    let contents = await fs.readFile(cFile.fd);
+    await exec(`clang ${cFile.path} -o ${exeFile.path}`);
+    try {
+        await exec(exeFile.path);
+    } catch (e) {
+        t.deepEqual(e.code, 7);
+    }
 });
