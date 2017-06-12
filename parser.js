f6c1be92bd0be8fb6860fd7080fe3d24a65d3659
Drew Gross <drew.a.gross@gmail.com>
2017-06-11 23:35:13 -0700

    Pur parser in it's own file

---
+++ parser.js
@@ -0,0 +1,57 @@
+const { alternative, sequence, terminal } = require('./parser-combinator.js');
+
+const parseProgram = (t, i) => parseProgramI(t, i);
+const parseStatement = (t, i) => parseStatementI(t, i);
+const parseFunction = (t, i) => parseFunctionI(t, i);
+const parseArgList = (t, i) => parseArgListI(t, i);
+const parseExpression = (t, i) => parseExpressionI(t, i);
+const parseProduct = (t, i) => parseProductI(t, i);
+
+// Grammar:
+// PROGRAM -> STATEMENT STATEMENT_SEPARATOR PROGRAM | return EXPRESSION
+// STATEMENT -> identifier = FUNCTION
+// FUNCTION -> ARG_LIST => EXPRESSION
+// ARG_LIST -> identifier, ARG_LIST | identifier
+// EXPRESSION -> PRODUCT | ( EXPRESSION ) | int
+// PRODUCT -> int * EXPRESSION | ( EXPRESSION ) * EXPRESSION
+
+const parseProgramI = alternative([
+    sequence('statement', [parseStatement, terminal('statementSeparator'), parseProgram]),
+    sequence('returnStatement', [terminal('return'), parseExpression]),
+]);
+
+const parseStatementI = sequence('assignment', [
+    terminal('identifier'),
+    terminal('assignment'),
+    parseFunction,
+]);
+
+const parseFunctionI = sequence('function', [parseArgList, terminal('fatArrow'), parseExpression]);
+
+const parseArgListI = alternative([
+    sequence('argList', [terminal('identifier'), terminal('comma'), parseArgList]),
+    terminal('identifier'),
+]);
+
+const parseExpression2 = sequence('bracketedExpression', [
+    terminal('leftBracket'),
+    parseExpression,
+    terminal('rightBracket'),
+]);
+const parseExpressionI = alternative([parseProduct, parseExpression2, terminal('number')]);
+
+const parseProduct1 = sequence('product1', [
+    terminal('number'),
+    terminal('product'),
+    parseExpression,
+]);
+const parseProduct2 = sequence('product2', [
+    terminal('leftBracket'),
+    parseExpression,
+    terminal('rightBracket'),
+    terminal('product'),
+    parseExpression,
+]);
+const parseProductI = alternative([parseProduct1, parseProduct2]);
+
+module.exports = parseProgram;
