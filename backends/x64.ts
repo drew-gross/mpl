08b662f229c277269411b0188e7e3d91a3b8fee9
Drew Gross <drew.a.gross@gmail.com>
2018-03-18 22:50:17 -0700

    Some dummy sample code

--- backends/x64.ts
+++ backends/x64.ts
@@ -2,9 +2,7 @@ import { exec } from 'child-process-promise';
 import { file as tmpFile } from 'tmp-promise';
 import execAndGetResult from '../util/execAndGetResult.js';
 
-export default {
-    name: 'x64',
-    toExectuable: ({}) => `
+const sample = `
 ; ----------------------------------------------------------------------------------------
 ; Writes "Hello, World" to the console using only system calls. Runs on 64-bit macOS only.
 ; ----------------------------------------------------------------------------------------
@@ -23,6 +21,20 @@ start:    mov       rax, 0x02000004         ; system call for write
 
           section   .data
 message:  db        "Hello, World", 10      ; note the newline at the end
+`
+
+export default {
+    name: 'x64',
+    toExectuable: ({}) => `
+    global start
+
+    section .text
+start: mov rdi, 7; exit code
+    mov rax, 0x02000001; system call for exit
+    syscall
+
+    section .data
+message: db "Must have writable segment", 10; newline mandatory. This exists to squelch dyld errors
 `,
     execute: async path => {
         const linkerInputPath = await tmpFile({ postfix: '.o' });
