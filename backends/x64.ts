6ca4ecaea2ac4c5a3fdf3e6e4ed322ace24c0f95
Drew Gross <drew.a.gross@gmail.com>
2018-03-19 22:00:56 -0700

    Adjust style

--- backends/x64.ts
+++ backends/x64.ts
@@ -1,3 +1,4 @@
+import { BackendInputs } from '../api.js';
 import { exec } from 'child-process-promise';
 import { file as tmpFile } from 'tmp-promise';
 import execAndGetResult from '../util/execAndGetResult.js';
@@ -21,21 +22,25 @@ start:    mov       rax, 0x02000004         ; system call for write
 
           section   .data
 message:  db        "Hello, World", 10      ; note the newline at the end
-`
+`;
 
 export default {
     name: 'x64',
-    toExectuable: ({}) => `
-    global start
+    toExectuable: ({ functions, program, globalDeclarations, stringLiterals }: BackendInputs) => {
+        return `
+global start
 
-    section .text
-start: mov rdi, 7; exit code
+section .text
+start:
+    mov rdi, 7; exit code
     mov rax, 0x02000001; system call for exit
     syscall
 
-    section .data
-message: db "Must have writable segment", 10; newline mandatory. This exists to squelch dyld errors
-`,
+section .data
+message:
+    db "Must have writable segment", 10; newline mandatory. This exists to squelch dyld errors
+`
+    },
     execute: async path => {
         const linkerInputPath = await tmpFile({ postfix: '.o' });
         const exePath = await tmpFile({ postfix: '.out' });
