fddc12d266e6690aa104aab2944ab61171df13d2
Drew Gross <drew.a.gross@gmail.com>
2019-06-15 22:36:48 -0700

    Shrink test

--- test.ts
+++ test.ts
@@ -2021,7 +2021,7 @@ test.only('Ordered Set To List After Removing', t => {
     });
 
     const inserted = [88, 97, 93, 99, 34, 94, 51];
-    const removed = [94, 67, 76, 86, 99, 65, 88, 97, 60, 91];
+    const removed = [94, 86, 99, 65, 88, 97, 60, 91];
 
     inserted.forEach(x => s.add(x));
     removed.forEach(x => s.remove(x));
