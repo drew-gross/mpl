d8e6b75c622f6e2c3a5d38c58c1cee8ab01a7e87
Drew Gross <drew.a.gross@gmail.com>
2019-11-10 22:15:58 -0800

    Hacks for writing to arguments is no longer necessary

--- controlFlowGraph.ts
+++ controlFlowGraph.ts
@@ -561,7 +561,7 @@ const removeDeadStores = (taf: ThreeAddressFunction, liveness: Set<Register>[]):
         // If there are written registers and none of them are live, omit the write.
         // TODO: Treat function result and arguments less special-casey somehow. Maybe put it into liveness computing. NOTE: Writes to arguments are not dead because length is implemented in a way where the arguments are destroyed and repaired (TODO: verify this). TODO: probably should check if any target is a register?
 
-        if (targets.length == 0 || taf.arguments.some(arg => registerIsEqual(targets[0], arg))) {
+        if (targets.length == 0) {
             newFunction.instructions.push(taf.instructions[i]);
         } else if (targets.length == 1) {
             const isLiveWrite = liveness[i + 1].has(targets[0]);
