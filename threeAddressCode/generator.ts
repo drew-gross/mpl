ababaeda3c582e720fdf9386ec3fb99725735532
Drew Gross <drew.a.gross@gmail.com>
2019-11-17 13:22:51 -0800

    Comments

--- threeAddressCode/generator.ts
+++ threeAddressCode/generator.ts
@@ -62,9 +62,11 @@ export type TargetThreeAddressStatement<TargetRegister> = { why: string } & (
     | { kind: 'callByRegister'; function: TargetRegister }
     | { kind: 'return' }
     // Stack Management
-    | { kind: 'loadStackOffset'; register: TargetRegister; offset: number } // TODO: This should be fused with stackStore probably
+    | { kind: 'loadStackOffset'; register: TargetRegister; offset: number } // TODO: This should be fused with stackLoad probably, currently this offsets by bytes and stackLoad offsets by works.
     | { kind: 'stackStore'; register: TargetRegister; offset: number }
     | { kind: 'stackLoad'; register: TargetRegister; offset: number }
+    //| { kind: 'stackReserve'; words: number }
+    //| { kind: 'stackRelease'; words: number }
     | { kind: 'push'; register: TargetRegister }
     | { kind: 'pop'; register: TargetRegister }
 );
