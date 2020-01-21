c665bd406a0f5d0746253c1afd1219f0bb061c9d
Drew Gross <drew.a.gross@gmail.com>
2020-01-20 16:25:10 -0800

    make an experimental loader mode

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,32 @@
 const path = require('path');
 
-module.exports = {
-    devtool: 'inline-source-map',
-    entry: {
-        mpl: './mpl.ts',
-        'mpl-loader': './mpl-loader.ts',
-    },
-    module: {
-        rules: [
-            {
-                exclude: /node_modules/,
-                test: /\.tsx?$/,
-                use: 'ts-loader',
-            },
-            {
-                exclude: /node_modules/,
-                test: /\.mpl$/,
-                use: path.resolve('tools/mpl-loader'),
-            },
-        ],
-    },
-    resolve: {
-        extensions: ['.tsx', '.ts', '.js'],
-    },
-    target: 'node',
+module.exports = env => {
+    const useDevLoader = env && 'experimental' in env && env.experimental === true;
+    return {
+        devtool: 'inline-source-map',
+        entry: {
+            mpl: './mpl.ts',
+            'mpl-loader': './mpl-loader.ts',
+        },
+        module: {
+            rules: [
+                {
+                    exclude: /node_modules/,
+                    test: /\.tsx?$/,
+                    use: 'ts-loader',
+                },
+                {
+                    exclude: /node_modules/,
+                    test: /\.mpl$/,
+                    use: useDevLoader
+                        ? path.resolve('tools/mpl-loader')
+                        : path.resolve('tools/mpl-loader-dev'),
+                },
+            ],
+        },
+        resolve: {
+            extensions: ['.tsx', '.ts', '.js'],
+        },
+        target: 'node',
+    };
 };
