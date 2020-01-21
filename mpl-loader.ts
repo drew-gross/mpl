cae5fddea0c1b81ebf61c894b5400ad97b45f06e
Drew Gross <drew.a.gross@gmail.com>
2020-01-20 16:36:02 -0800

    Errors

--- mpl-loader.ts
+++ mpl-loader.ts
@@ -1,1 +1,14 @@
-export default (source: string) => source;
+import { compile } from './frontend';
+import jsBackend from './backends/js';
+
+export default function(source: string) {
+    const frontendOutput = compile(source);
+    if (
+        'parseErrors' in frontendOutput ||
+        'typeErrors' in frontendOutput ||
+        'kind' in frontendOutput ||
+        'internalError' in frontendOutput
+    ) {
+        this.emitError('ooops');
+    }
+}
