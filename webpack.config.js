8ef9f57d1da571b1f84c5d28ccb0bb28fc7103cc
Drew Gross <drew.a.gross@gmail.com>
2020-03-25 20:53:58 -0700

    experimental

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,36 @@
 const path = require('path');
 
-module.exports = {
-    entry: {
-        mpl: './mpl.ts',
-        'mpl-loader-raw': './mpl-loader.ts',
-    },
-    target: 'node',
-    devtool: false,
-    module: {
-        rules: [
-            {
-                test: /\.ts$/,
-                use: 'ts-loader',
-                exclude: /node_modules/,
-            },
-            {
-                test: /\.mpl$/,
-                use: './bin/mpl-loader.js',
-                exclude: /node_modules/,
-            },
-        ],
-    },
-    resolve: { extensions: ['.ts', '.js'] },
-    output: {
-        filename: '[name].js',
-        path: path.resolve(__dirname, 'bin'),
-        library: 'mplLoader',
-        libraryTarget: 'umd',
-    },
+module.exports = env => {
+    filename = env.commit ? '[name].js' : '[name]-experimental.js';
+    console.log(env);
+
+    return {
+        entry: {
+            mpl: './mpl.ts',
+            'mpl-loader-raw': './mpl-loader.ts',
+        },
+        target: 'node',
+        devtool: false,
+        module: {
+            rules: [
+                {
+                    test: /\.ts$/,
+                    use: 'ts-loader',
+                    exclude: /node_modules/,
+                },
+                {
+                    test: /\.mpl$/,
+                    use: './bin/mpl-loader.js',
+                    exclude: /node_modules/,
+                },
+            ],
+        },
+        resolve: { extensions: ['.ts', '.js'] },
+        output: {
+            filename,
+            path: path.resolve(__dirname, 'bin'),
+            library: 'mplLoader',
+            libraryTarget: 'umd',
+        },
+    };
 };
