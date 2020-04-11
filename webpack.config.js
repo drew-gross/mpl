536c54016ded4ef4acd328d0b05d188ba3844dcf
Drew Gross <drew.a.gross@gmail.com>
2020-04-11 15:41:02 -0700

    Compile the tests with webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,22 @@
-const path = require('path');
+const resolve = require('path').resolve;
 
 module.exports = env => {
     let filename = '[name]-experimental.js';
+    let path = resolve(__dirname, 'bin');
     if (env.commit) {
         filename = '[name].js';
     } else if (env.experimental) {
         filename = '[name]-super-experimental.js';
+    } else if (env.tests) {
+        filename = '[name].js';
+        path = resolve(__dirname, 'built');
     }
 
     return {
         entry: {
             mpl: './mpl.ts',
             'mpl-loader-raw': './mpl-loader.ts',
+            test: './test.ts',
         },
         target: 'node',
         devtool: false,
@@ -33,11 +38,7 @@ module.exports = env => {
             ],
         },
         resolve: { extensions: ['.ts', '.js'] },
-        output: {
-            filename,
-            path: path.resolve(__dirname, 'bin'),
-            library: 'mplLoader',
-            libraryTarget: 'umd',
-        },
+        output: { filename, path, library: 'mplLoader', libraryTarget: 'umd' },
+        externals: { ava: 'ava' },
     };
 };
