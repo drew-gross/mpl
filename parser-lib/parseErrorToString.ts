c53c789ce417b869dee554def5514f21bcf3fbdd
Drew Gross <drew.a.gross@gmail.com>
2024-05-20 17:57:19 -0700

    format

--- parser-lib/parseErrorToString.ts
+++ parser-lib/parseErrorToString.ts
@@ -4,5 +4,5 @@ import join from '../util/join';
 export default ({ expected, found, sourceLocation, whileParsing }: ParseError): string => {
     const line = sourceLocation.line;
     const col = sourceLocation.column;
-    return `Expected ${expected} but found ${found} at ${line}:${col} (${join(whileParsing, "->")})`;
+    return `Expected ${expected} but found ${found} at ${line}:${col} (${join(whileParsing, '->')})`;
 };
